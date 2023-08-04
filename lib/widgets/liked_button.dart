a3a00497a060b1acd48620deed7db768eac39e12
Sebastian Słowik <s.slowik@praktyki.ideo.pl>
2023-08-04 09:42:23 +0200

    changed style of liked button

--- lib/widgets/liked_button.dart
+++ lib/widgets/liked_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:testproject/cubit/favorites_cubit.dart';
 import 'package:testproject/models/book.dart';
+import 'package:testproject/styles/colors.dart';
 
 class LikedButton extends StatelessWidget {
   final Book book;
@@ -18,13 +19,48 @@ class LikedButton extends StatelessWidget {
 
     return Align(
       alignment: Alignment.centerRight,
-      child: ElevatedButton(
-        onPressed: () => isLiked
-            ? favoritesCubit.removeFromFavorites(book)
-            : favoritesCubit.addToFavorites(book),
-        child: isLiked
-            ? const Icon(Icons.thumb_up)
-            : const Icon(Icons.thumb_up_outlined),
+      child: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 24,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: shadowColor,
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () => isLiked
+                ? favoritesCubit.removeFromFavorites(book)
+                : favoritesCubit.addToFavorites(book),
+            style: ElevatedButton.styleFrom(
+              shadowColor: shadowColor,
+              elevation: 0,
+              surfaceTintColor: whiteColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            child: isLiked
+                ? const Icon(Icons.thumb_up)
+                : const Icon(
+                    Icons.thumb_up_outlined,
+                    color: lightGreyColor,
+                  ),
+          ),
+        ],
       ),
     );
   }
